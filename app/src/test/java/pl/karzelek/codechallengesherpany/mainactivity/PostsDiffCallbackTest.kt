9ab91b1a556ebb31b2803de629b141c8754de8df
Piotr Karzełek <piotr@karzelek.pl>
2020-10-17 15:31:31 +0200

    add tests for checking the PostsDiffCallback lists sizes

--- app/src/test/java/pl/karzelek/codechallengesherpany/mainactivity/PostsDiffCallbackTest.kt
+++ app/src/test/java/pl/karzelek/codechallengesherpany/mainactivity/PostsDiffCallbackTest.kt
@@ -1,7 +1,6 @@
 package pl.karzelek.codechallengesherpany.mainactivity
 
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Test
 import pl.karzelek.codechallengesherpany.db.PostWithUser
 import pl.karzelek.codechallengesherpany.entities.Post
@@ -23,10 +22,10 @@ class PostsDiffCallbackTest {
     @Test
     fun reversedList() {
         //given
-        val list1 = listOf(PostWithUser(post1, user), PostWithUser(post2, user))
+        val list = listOf(PostWithUser(post1, user), PostWithUser(post2, user))
         val reversedList = listOf(PostWithUser(post2, user), PostWithUser(post1, user))
         //when
-        val postsDiffCallback = PostsDiffCallback(list1, reversedList)
+        val postsDiffCallback = PostsDiffCallback(list, reversedList)
         //then
         assertTrue(postsDiffCallback.areItemsTheSame(0, 1))
         assertTrue(postsDiffCallback.areContentsTheSame(0, 1))
@@ -34,6 +33,18 @@ class PostsDiffCallbackTest {
         assertTrue(postsDiffCallback.areContentsTheSame(1, 0))
         assertFalse(postsDiffCallback.areItemsTheSame(0, 0))
         assertFalse(postsDiffCallback.areContentsTheSame(0, 0))
+    }
+
+    @Test
+    fun checkSize() {
+        //given
+        val list1element = listOf(PostWithUser(post1, user))
+        val list2elements = listOf(PostWithUser(post1, user), PostWithUser(post2, user))
+        //when
+        val diffCallback = PostsDiffCallback(list1element, list2elements)
+        //then
+        assertEquals(1, diffCallback.oldListSize)
+        assertEquals(2, diffCallback.newListSize)
     }
 
     companion object {
